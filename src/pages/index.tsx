fcfe1f46d0fc7c9b33615294fa4da89b41d17b3b
seufernandez <seufernandezdev@gmail.com>
2021-08-24 03:28:26 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,43 @@
+import { GetStaticProps } from 'next';
+
+import { getPrismicClient } from '../services/prismic';
+
+import commonStyles from '../styles/common.module.scss';
+import styles from './home.module.scss';
+
+interface Post {
+  uid?: string;
+  first_publication_date: string | null;
+  data: {
+    title: string;
+    subtitle: string;
+    author: string;
+  };
+}
+
+interface PostPagination {
+  next_page: string;
+  results: Post[];
+}
+
+interface HomeProps {
+  postsPagination: PostPagination;
+}
+
+export default function Home() {
+  // TODO
+  return (
+    <>
+      <h1>Posts</h1>
+    </>
+  );
+}
+
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(TODO);
+
+  return {
+    props: {},
+  };
+};
