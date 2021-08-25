c1f0703dd212872b248c7abe6f7fc5e37474da5a
seufernandez <seufernandezdev@gmail.com>
2021-08-25 17:55:28 -0300

    main styles added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { GetStaticProps } from 'next';
-
 import Prismic from '@prismicio/client';
-import { RichText } from 'prismic-dom';
 import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -25,25 +27,41 @@ interface PostPagination {
 }
 
 interface HomeProps {
-  // postsPagination: PostPagination;
-  posts;
+  postsPagination: PostPagination;
 }
 
-export default function Home({ posts }) {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
   // TODO
   // console.log(posts);
 
   return (
     <>
+      <Head>
+        <title>Bityf94 | Home</title>
+      </Head>
+
       <Header />
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          {posts.map(post => (
+          {postsPagination.results.map(post => (
             <>
-              <h1>{post.title}</h1>
-              <h3>{post.updatedAt}</h3>
-              <p>{post.subtitle}</p>
+              <Link key={post.uid} href={`/posts/${post.uid}`}>
+                <a>
+                  <h1>{post.data.title}</h1>
+                  <p>{post.data.subtitle}</p>
+                  <ul>
+                    <li>
+                      <FiCalendar />
+                      {post.first_publication_date}
+                    </li>
+                    <li>
+                      <FiUser />
+                      {post.data.author}
+                    </li>
+                  </ul>
+                </a>
+              </Link>
             </>
           ))}
         </div>
@@ -63,24 +81,29 @@ export const getStaticProps: GetStaticProps = async () => {
   console.log(JSON.stringify(response, null, 2));
 
   const posts = response.results.map(post => {
-    console.log(JSON.stringify(post, null, 2));
     return {
-      slug: post.uid,
-      title: post.data.title,
-      subtitle: post.data.subtitle,
-      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
-        'en-US',
-        {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        }
-      ),
+      uid: post.uid,
+      first_publication_date: new Date(
+        post.first_publication_date
+      ).toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
     };
   });
 
-  // console.log(JSON.stringify(posts, null, 2));
+  const postsPagination = {
+    next_page: response.next_page,
+    results: posts,
+  };
+
   return {
-    props: { posts },
+    props: { postsPagination },
   };
 };
